50328f9c072d0908c5650cdf4935f3079816ee8a
Javeria021 <javeriaalam1122@gmail.com>
2024-05-21 10:50:20 +0500

    Dashboardchangings-Part2

--- src/pages/management/ProductManagement.tsx
+++ src/pages/management/ProductManagement.tsx
@@ -3,8 +3,7 @@ import AdminSidebar from "../../components/AdminSidebar";
 
 const img =
   "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8c2hvZXN8ZW58MHx8MHx8&w=1000&q=804";
-
-const ProductManagement = () => {
+  const ProductManagement = () => {
   const [name, setName] = useState<string>("Puma Shoes");
   const [price, setPrice] = useState<number>(2000);
   const [stock, setStock] = useState<number>(10);
@@ -41,7 +40,7 @@ const ProductManagement = () => {
       <AdminSidebar />
       <main className="product-management">
         <section>
-          <strong>ID - asnmdkasndmsan</strong>
+          <strong>ID - Javeriaalam</strong>
           <img src={photo} alt="Product" />
           <p>{name}</p>
           {stock > 0 ? (
@@ -57,24 +56,34 @@ const ProductManagement = () => {
             <div className="manage-header">
               <div style={{display:"flex",justifyContent: "space-between"}}>
               <h2>Manage</h2>
-              <button className="close-button" onClick={() => console.log("Close button clicked")}>
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="icon icon-tabler icon-tabler-x"
-                  width="24"
-                  height="24"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  fill="none"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-                  <line x1="18" y1="6" x2="6" y2="18" />
-                  <line x1="6" y1="6" x2="18" y2="18" />
-                </svg>
-              </button>
+              <button 
+              className="close-button" 
+              onClick={() => console.log("Close button clicked")}
+              style={{ 
+                backgroundColor: "transparent", 
+                border: "none",
+                outline: "none",
+                cursor: "pointer"
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="icon icon-tabler icon-tabler-x"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </button>
+
               </div>
               
             </div>
